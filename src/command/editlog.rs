74014219dcb85458afe42a497e325f40d0ca8394
AXON <axonasif@gmail.com>
2021-05-07 04:40:13 +0000

    fix: Leftover bugs in editlog

--- src/command/editlog.rs
+++ src/command/editlog.rs
@@ -4,14 +4,13 @@ use super::*;
 pub async fn editlog(_ctx: &Context, _msg: &Message) -> CommandResult {
     let ref_msg = &_msg.referenced_message;
     // Use contentsafe options
-    let settings = {
-        ContentSafeOptions::default()
-            .clean_channel(false)
-            .clean_role(true)
-            .clean_user(false)
-    };
+    let settings = { ContentSafeOptions::default().clean_user(true) };
     if ref_msg.is_some() {
-        if _msg.edited_timestamp.is_some() {
+        if ref_msg
+            .as_ref()
+            .map(|x| x.edited_timestamp.is_some())
+            .unwrap()
+        {
             let ref_msg_id = ref_msg.as_ref().unwrap().id;
             let dbnode = Database::from("msgcache".to_string()).await;
             let content = content_safe(
@@ -24,7 +23,12 @@ pub async fn editlog(_ctx: &Context, _msg: &Message) -> CommandResult {
             )
             .await;
 
-            _msg.reply(&_ctx.http, &content).await?;
+            ref_msg
+                .as_ref()
+                .map(|x| x.reply(&_ctx.http, &content))
+                .unwrap()
+                .await?;
+            // _msg.reply(&_ctx.http, &content).await?;
         } else {
             _msg.reply(&_ctx.http, "Not an edited message").await?;
         }
