33a03181c357b061b140f19a9d1816b9269471b9
AXON <axonasif@gmail.com>
2021-05-07 02:42:21 +0000

    fix: Bug in editlog cmd

--- src/command/editlog.rs
+++ src/command/editlog.rs
@@ -3,11 +3,14 @@ use super::*;
 #[command]
 pub async fn editlog(_ctx: &Context, _msg: &Message) -> CommandResult {
     let ref_msg = &_msg.referenced_message;
-    let ref_msg_id = ref_msg.as_ref().unwrap().id;
-    let dbnode = Database::from("msgcache".to_string()).await;
-    let content = dbnode.fetch_msg(ref_msg_id).await.replace("---MSG_TYPE---", "Edited:");
 
     if ref_msg.is_some() {
+        let ref_msg_id = ref_msg.as_ref().unwrap().id;
+        let dbnode = Database::from("msgcache".to_string()).await;
+        let content = dbnode
+            .fetch_msg(ref_msg_id)
+            .await
+            .replace("---MSG_TYPE---", "Edited:");
         _msg.reply(&_ctx.http, &content).await?;
     } else {
         _msg.reply(
