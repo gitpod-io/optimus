11b00e13c9acfa38bc435cbc3091b49ade8e6e5d
AXON <axonasif@gmail.com>
2021-05-16 11:37:33 +0000

    feat: New command `av`

---
+++ src/command/av.rs
@@ -0,0 +1,32 @@
+use super::*;
+
+#[command]
+pub async fn av(_ctx: &Context, _msg: &Message, mut _args: Args) -> CommandResult {
+    let user = Parse::user(&_msg, &_args);
+    let guild_id = &_msg.guild_id.unwrap();
+    let user_data = &_ctx
+        .http
+        .get_member(*guild_id.as_u64(), user)
+        .await
+        .unwrap();
+
+    _msg.channel_id
+        .send_message(&_ctx.http, |m| {
+            m.embed(|e| {
+                e.title(format!("**{}**", &user_data.display_name()));
+                e.url(format!(
+                    "https://images.google.com/searchbyimage?image_url={}",
+                    &user_data.user.face()
+                ));
+                e.image(&user_data.user.face());
+
+                e
+            });
+
+            m
+        })
+        .await
+        .unwrap();
+
+    Ok(())
+}
