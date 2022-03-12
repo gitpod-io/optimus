c7c31278b9c5f3c441326aaa2048c63504d09ce6
AXON <axonasif@gmail.com>
2022-03-12 18:19:50 +0600

    fix: Auto unarchiving bug

--- src/event/thread_update.rs
+++ src/event/thread_update.rs
@@ -5,7 +5,7 @@ use super::*;
 // since there is no reliable way to detect who triggered thread_update
 pub async fn responder(_ctx: Context, _thread: GuildChannel) {
     let thread_type = {
-        if _thread.name.contains("✅") || _thread.name.contains("❓") {
+        if _thread.name.starts_with("✅") || _thread.name.starts_with("❓") {
             "question"
         } else {
             "thread"
@@ -13,29 +13,31 @@ pub async fn responder(_ctx: Context, _thread: GuildChannel) {
     };
 
     let last = _thread.last_message_id.unwrap();
-    let is_self = &_ctx
+    let last_msg = &_ctx
         .http
         .get_message(*_thread.id.as_u64(), *last.as_u64())
         .await
         .unwrap();
 
-    if !is_self.is_own(&_ctx.cache).await && _thread.thread_metadata.unwrap().archived {
+    let no_activity_message = format!("> This {} did not have any recent activity or wasn't normally closed. Feel free to `/close` it or post an update if anything is unresolved.", thread_type);
+
+    if !last_msg.is_own(&_ctx.cache).await && _thread.thread_metadata.unwrap().archived {
         _thread
-                .send_message(&_ctx, |m| {
-                    m.content(format!("> This {} did not have any recent activity or wasn't normally closed. Feel free to `/close` it or post an update if anything is unresolved.", thread_type)).components(|c| {
-                        c.create_action_row(|ar| {
-                            ar.create_button(|button| {
-                                button
-                                    .style(ButtonStyle::Success)
-                                    .label("Close")
-                                    .custom_id("gitpod_close_issue")
-                                    .emoji(ReactionType::Unicode("✉️".to_string()))
-                            })
+            .send_message(&_ctx, |m| {
+                m.content(&no_activity_message).components(|c| {
+                    c.create_action_row(|ar| {
+                        ar.create_button(|button| {
+                            button
+                                .style(ButtonStyle::Success)
+                                .label("Close")
+                                .custom_id("gitpod_close_issue")
+                                .emoji(ReactionType::Unicode("✉️".to_string()))
                         })
                     })
                 })
-                .await
-                .unwrap();
+            })
+            .await
+            .unwrap();
         // let tp = _thread.id.get_thread_members(&_ctx.http).await.unwrap();
 
         // for mem in tp.iter() {
@@ -52,7 +54,10 @@ pub async fn responder(_ctx: Context, _thread: GuildChannel) {
         //     .edit_thread(&_ctx, |t| t.archived(true))
         //     .await
         //     .unwrap();
-    } else if is_self.is_own(&_ctx.cache).await && _thread.thread_metadata.unwrap().archived {
+    } else if last_msg.is_own(&_ctx.cache).await
+        && last_msg.content == no_activity_message
+        && _thread.thread_metadata.unwrap().archived
+    {
         _thread
             .edit_thread(&_ctx.http, |t| t.archived(false))
             .await
