b648819a1a61876c421c8d47c973e95b10b00a47
Antonio Nardella <antonio.nardella@iota.org>
2022-08-01 16:31:49 +0200

    Onboarding to Onboarded

--- src/event/interaction_create.rs
+++ src/event/interaction_create.rs
@@ -406,7 +406,7 @@ pub async fn responder(ctx: Context, interaction: Interaction) {
 
                     // Get user and check if user already went threw onboarding once
                     let mut member = mci.member.clone().unwrap();
-                    let member_role = get_role(&mci, ctx, "Onboarding").await;
+                    let member_role = get_role(&mci, ctx, "Onboarded").await;
                     
                     let never_introduced = !member.roles.iter().any(|x| x == &member_role.id);
 
