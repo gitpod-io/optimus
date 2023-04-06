3a3b053ce8cc7b305190f0bcd62049e4f7a75caa
AXON <axonasif@gmail.com>
2023-04-06 17:51:37 +0600

    refactor: Consolidate ready event

--- src/event/ready.rs
+++ src/event/ready.rs
@@ -1,20 +1,22 @@
-use serenity::model::{
-    prelude::command::{CommandOptionType, CommandType},
-    Permissions,
+use color_eyre::Report;
+use serenity::{
+    futures::StreamExt,
+    model::{
+        application::{command::Command, component::ButtonStyle},
+        prelude::{
+            command::{CommandOptionType, CommandType},
+            GuildInfo, ReactionType,
+        },
+        Permissions,
+    },
 };
 
 use super::*;
 
-pub async fn responder(_ctx: &Context, ready: Ready) -> Result<()> {
-    println!("{} is connected!", ready.user.name);
-    _ctx.set_activity(Activity::watching("The pods on Gitpod!"))
-        .await;
-
-    let guilds = &ready.user.guilds(&_ctx.http).await?;
-
-    for guild in guilds {
-        // let commands =
-        GuildId::set_application_commands(&guild.id, &_ctx.http, |commands| {
+struct Init;
+impl Init {
+    async fn set_app_cmds(guild: &GuildInfo, ctx: &Context) -> Result<Vec<Command>, Report> {
+        let cmds = GuildId::set_application_commands(&guild.id, &ctx.http, |commands| {
             commands.create_application_command(|command| {
                 command.name("close").description("Close a question")
             });
@@ -55,16 +57,57 @@ pub async fn responder(_ctx: &Context, ready: Ready) -> Result<()> {
             commands
         })
         .await?;
+
+        Ok(cmds)
     }
 
-    // Not useful in multi-guild context
-    // println!(
-    //     "Now I have these application commands: {}",
-    //     commands
-    //         .into_iter()
-    //         .map(|x| format!("{} ", x.name))
-    //         .collect::<String>()
-    // );
+    async fn install_getting_started_message(ctx: &Context) -> Result<()> {
+        let placeholder_text = "**Press the button below** 👇 to gain access to the server";
+
+        if let Some(config) = crate::BOT_CONFIG.get() && let Some(channels) = &config.discord.channels
+        && let Some(getting_started_channel) = channels.getting_started_channel_id {
+
+            let mut cursor = getting_started_channel.messages_iter(&ctx.http).boxed();
+            while let Some(message_result) = cursor.next().await {
+                if let Ok(message) = message_result &&message.content == *placeholder_text {
+                        return Ok(())
+                }
+            }
+
+            getting_started_channel
+                .send_message(&ctx.http, |m| {
+                    m.content(placeholder_text);
+                    m.components(|c| {
+                        c.create_action_row(|a| {
+                            a.create_button(|b| {
+                                b.label("Let's go")
+                                    .custom_id("getting_started_letsgo")
+                                    .style(ButtonStyle::Primary)
+                                    .emoji(ReactionType::Unicode("🙌".to_string()))
+                            })
+                        })
+                    });
+                    m
+                })
+                .await?;
+        }
+
+        Ok(())
+    }
+}
+
+// Main entrypoint
+pub async fn responder(ctx: &Context, ready: Ready) -> Result<()> {
+    println!("{} is connected!", ready.user.name);
+    ctx.set_activity(Activity::watching("The pods on Gitpod!"))
+        .await;
+
+    let guilds = &ready.user.guilds(&ctx.http).await?;
+    for guild in guilds {
+        Init::set_app_cmds(guild, ctx).await?;
+    }
+
+    Init::install_getting_started_message(ctx).await?;
 
     Ok(())
 }
