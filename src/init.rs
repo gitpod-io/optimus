f6354a506f2df662e36ce3f5c834630db2cf491d
AXON <axonasif@gmail.com>
2023-04-11 06:47:13 +0600

    feat: build and publish docker container

--- src/init.rs
+++ src/init.rs
@@ -1,19 +1,73 @@
 use color_eyre::eyre::{eyre, Result};
-use meilisearch_sdk::indexes::Index;
-use meilisearch_sdk::{client::Client as MeiliClient, settings::Settings};
+use meilisearch_sdk::{client::Client as MeiliClient, indexes::Index, settings::Settings};
 use once_cell::sync::OnceCell;
+use sysinfo::{System, SystemExt};
+use tokio::time::{sleep, Duration, Instant};
+use tracing::{event, Level};
 
 use crate::config::MeilisearchConfig;
 
 pub static MEILICLIENT_THREAD_INDEX: OnceCell<Index> = OnceCell::new();
 
 pub async fn meilisearch(meili: &MeilisearchConfig) -> Result<()> {
-    let mclient = MeiliClient::new(&meili.api_endpoint, Some(&meili.api_key));
+    let mut system = System::new_all();
+    system.refresh_processes();
+    if system
+        .processes_by_name("meilisearch")
+        .peekable()
+        .peek()
+        .is_none()
+    {
+        // Get executable path and parent dir
+        let exec_path = std::env::current_exe()?;
+        let exec_parent_dir = exec_path
+            .parent()
+            .ok_or_else(|| eyre!("Failed to get parent dir of self"))?;
+
+        let mut server_cmd = meili.server_cmd.clone();
+        // Add db-path if not specified
+        if !&server_cmd.contains(&"--db-path".to_owned()) {
+            let db_dir = exec_parent_dir
+                .join("data.ms")
+                .to_string_lossy()
+                .to_string();
+            server_cmd.extend(vec!["--db-path".to_owned(), db_dir]);
+        }
+
+        // Start the meilisearch server
+        std::process::Command::new(&server_cmd[0])
+            .args(&server_cmd[1..])
+            .args(["--log-level", "WARN"])
+            .args(["--master-key", &meili.master_key])
+            .spawn()?;
+
+        // Await for the server to be fully up
+        let tmp_client = reqwest::Client::new();
+        let start = Instant::now();
+        while tmp_client
+            .get(format!("{}/{}", &meili.api_endpoint, "health"))
+            .send()
+            .await
+            .and_then(|op| op.error_for_status())
+            .is_err()
+        {
+            sleep(Duration::from_millis(200)).await;
+
+            if start.elapsed() > Duration::from_secs(15) {
+                return Err(eyre!("Meilisearch server start timeout"));
+            }
+        }
+    } else {
+        event!(Level::WARN, "Meilisearch server is already running");
+    }
+
+    let mclient = MeiliClient::new(&meili.api_endpoint, Some(&meili.master_key));
     let msettings = Settings::new()
         .with_searchable_attributes(["title", "messages", "tags", "author_id", "id"])
         .with_filterable_attributes(["timestamp", "tags"])
         .with_sortable_attributes(["timestamp"])
         .with_distinct_attribute("title");
+
     let threads_index_db = {
         let index_uid = "threads";
         if let Ok(res) = mclient.get_index(index_uid).await {
